542075aed6c9ff37a9f9abd7b1dbea56605b3051
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-05-10 17:50:12 +0530

    Products endpoint for CRUD

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -1,14 +1,39 @@
 'use strict';
 
-// Importing required modules
+// Import required modules
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 const categoryController = require('../controllers/categoryController');
+const productController = require('../controllers/productController');
+const productUpload = upload.fields([
+  { name: 'thumbnail', maxCount: 1 },
+  { name: 'images', maxCount: 10 },
+]);
 
-// Routes for categories
+// <-- Routes for categories -->
+// Get all categories
 router.get('/categories', categoryController.categories);
+
+// Add a new category
 router.post('/categories', categoryController.addCategory);
+
+// Delete a category by ID
 router.delete('/categories/:id', categoryController.deleteCategoryById);
 
-// Exporting router to use in the main application
+// <-- Routes for products -->
+// Get products with optional filtering and pagination
+router.get('/', productController.getProducts);
+
+// Add a new product
+router.post('/', productUpload, productController.addProduct);
+
+// Edit an existing product
+router.patch('/:id', productUpload, productController.editProduct);
+
+// Delete a product by ID
+router.delete('/:id', productController.deleteProduct);
+
 module.exports = router;
