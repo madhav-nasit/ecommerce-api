f8946ff773d6bceac8b5b9bf83572c57be001c16
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-05-23 16:14:25 +0530

    Move db operations into userRepository

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -6,30 +6,23 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/userModel');
+const UserRepository = require('../repositories/userRepository');
 const secret = process.env.SECRET_KEY;
 
 /**
  * Registers a new user.
  */
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
   try {
-    const newUser = new User(req.body);
-    newUser.password = bcrypt.hashSync(req.body.password, 10);
-    const user = await newUser.save();
-    // Omit password field from the response
+    const userData = { ...req.body, password: bcrypt.hashSync(req.body.password, 10) };
+    const user = await UserRepository.createUser(userData);
     user.password = undefined;
-    return res.send(user);
+    res.status(200).json(user);
   } catch (err) {
-    console.log('Error', err);
-    if (err && err?.code === 11000) {
-      return res.status(400).send({
-        message: 'User already exists with given email.',
-      });
+    if (err && err.code === 11000) {
+      next('User already exists with given email.');
     } else {
-      return res.status(500).send({
-        message: err?.message || 'Error occurred while creating user.',
-      });
+      next('Error occurred while creating user.');
     }
   }
 };
@@ -37,28 +30,25 @@ const signUp = async (req, res) => {
 /**
  * Authenticates a user.
  */
-const signIn = async (req, res) => {
+const signIn = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email }).exec();
+    const user = await UserRepository.findUserByEmail(req.body.email);
     if (!user || !user.comparePassword(req.body.password)) {
-      return res.status(400).json({ message: 'Authentication failed. Invalid email or password.' });
+      next('Authentication failed. Invalid email or password.');
+    } else {
+      user.password = undefined;
+      const token = jwt.sign(
+        { email: user.email, firstName: user.firstName, lastName: user.lastName, _id: user._id },
+        secret,
+        { expiresIn: '1h' },
+      );
+      res.status(200).json({
+        ...user.toObject(),
+        token: token,
+      });
     }
-    // Omit password field from the response
-    user.password = undefined;
-    // Generate JWT token
-    const token = jwt.sign(
-      { email: user.email, firstName: user.firstName, lastName: user.lastName, _id: user._id },
-      secret,
-      { expiresIn: '1h' },
-    );
-    return res.json({
-      ...user.toObject(),
-      token: token,
-    });
   } catch (err) {
-    return res.status(500).send({
-      message: err?.message || 'Error occurred while authenticating user.',
-    });
+    next('Error occurred while authenticating user.');
   }
 };
 
@@ -69,45 +59,41 @@ const loginRequired = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    throw new Error('Unauthorized user!!');
-  }
-
-  try {
-    const decoded = await jwt.verify(token, secret);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Unauthorized user!!' });
+    next('Unauthorized user!!');
+  } else {
+    try {
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(401);
+      next('Unauthorized user!!');
+    }
   }
 };
 
 /**
  * Fetches user profile.
  */
-const profile = async (req, res) => {
+const profile = async (req, res, next) => {
   try {
-    const currentUserID = req.user._id;
-    const user = await User.findById(currentUserID).exec();
+    const user = await UserRepository.findUserById(req.user._id);
     user.password = undefined;
-    return res.send(user);
+    res.send(user);
   } catch (err) {
-    return res.status(500).send({
-      message: 'Error occurred while getting user.',
-    });
+    next('Error occurred while getting user.');
   }
 };
 
-const getUsers = async (req, res) => {
+/**
+ * Fetches all users except the current user.
+ */
+const getUsers = async (req, res, next) => {
   try {
-    const currentUserID = req.user._id;
-    const users = await User.find({ _id: { $ne: currentUserID } })
-      .select('firstName lastName _id')
-      .exec();
-    return res.send(users);
+    const users = await UserRepository.findAllUsersExcept(req.user._id);
+    res.send(users);
   } catch (err) {
-    return res.status(500).send({
-      message: 'Error occurred while getting users.',
-    });
+    next('Error occurred while getting users.');
   }
 };
 
---
+++ app/repositories/userRepository.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+const User = require('../models/userModel');
+
+/**
+ * Create a new user.
+ * @param {Object} userData - The user data to be saved.
+ * @returns {Promise<Object>} - A promise that resolves to the saved user object.
+ */
+const createUser = async (userData) => {
+  const newUser = new User(userData);
+  return await newUser.save();
+};
+
+/**
+ * Finds a user by email.
+ * @param {String} email - The email of the user.
+ * @returns {Object|null} The user object or null if not found.
+ */
+const findUserByEmail = async (email) => {
+  return await User.findOne({ email }).exec();
+};
+
+/**
+ * Finds a user by ID.
+ * @param {String} userId - The ID of the user.
+ * @returns {Object|null} The user object or null if not found.
+ */
+const findUserById = async (userId) => {
+  return await User.findById(userId).exec();
+};
+
+/**
+ * Finds all users except the current user.
+ * @param {String} currentUserID - The ID of the current user.
+ * @returns {Promise<Array>} - A promise that resolves to an array of user objects.
+ */
+const findAllUsersExcept = async (currentUserID) => {
+  return await User.find({ _id: { $ne: currentUserID } })
+    .select('firstName lastName _id')
+    .exec();
+};
+
+module.exports = {
+  createUser,
+  findUserByEmail,
+  findUserById,
+  findAllUsersExcept,
+};
